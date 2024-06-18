1a744a5b37f731d81938cec778fb42ebff663307
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-06-18 19:49:38 -0300

    app.py adicionado tags nas rotas para identificação.

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI(title='FastAPI do Zero')
 
 
-@app.get('/')
+@app.get('/', tags=['Rota Raiz'])
 def read_root():
     return {'message': 'Olá Mundo!'}
 
 
-@app.get('/page1')
+@app.get('/page1', tags=['Página 1'])
 def page1():
     return {'message': 'Lendo Página 1'}
