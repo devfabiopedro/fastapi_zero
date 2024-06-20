3bc877106a41bc1ae1488ae55b059c4647536f25
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-06-19 23:01:22 -0300

    Atualizações da aula 3

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,13 +1,35 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
+from fastapi.responses import HTMLResponse
+
+from fastapi_zero.schemas import Message
 
 app = FastAPI(title='FastAPI do Zero')
 
 
-@app.get('/', tags=['Rota Raiz'])
+@app.get(
+    '/',
+    tags=['Endpoint Raiz'],
+    status_code=status.HTTP_200_OK,
+    response_model=Message,
+)
 def read_root():
     return {'message': 'Olá Mundo!'}
 
 
-@app.get('/page1', tags=['Página 1'])
-def page1():
-    return {'message': 'Lendo Página 1'}
+@app.get(
+    '/olamundo',
+    tags=['Endpoint Olá Mundo HTML'],
+    status_code=status.HTTP_200_OK,
+    response_class=HTMLResponse,
+)
+def ola_mundo_html():
+    return """
+    <html>
+        <head>
+            <title>Nosso Olá Mundo</title>
+        </head>
+        <body>
+            <h2>Olá Mundo!</h2>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -13,7 +13,7 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
 
 
-def test_navegar_page1_deve_retornar_mensagem_lendo_pagina1():
-    response = client.get('/page1')  # Act
+def test_olamundo_deve_retornar_template_html_escrito_Ola_Mundo():
+    response = client.get('/olamundo')  # Act
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Lendo Página 1'}  # Assert
+    assert "Olá Mundo!" in response.text
