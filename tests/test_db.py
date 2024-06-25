305f6b3f5bf35e11d422943973aa5a7dddd9b03b
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-06-25 09:10:53 -0300

    Arquivo de testDb atualizado

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,15 @@
-from sqlalchemy import select
+from sqlalchemy import func, select
 
 from fastapi_zero.models import User
 
 
 def test_create_user(session):
-    new_user = User(username='alice', password='secret', email='teste@test')
+    new_user = User(
+        username='alice',
+        password='secret',
+        email='teste@test',
+        updated_at=func.now(),
+    )
     session.add(new_user)
     session.commit()
 
