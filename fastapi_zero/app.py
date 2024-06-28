51c2bf67437ad8e3b6a33ccd897a8722977147c5
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-06-27 22:14:00 -0300

    Endpoints usando o banco de dados real e com testes implementados

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,18 +1,13 @@
-from fastapi import FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
+from sqlalchemy import select
+from sqlalchemy.orm import Session
 
-from fastapi_zero.schemas import (
-    Message,
-    UserDB,
-    UserList,
-    UserPublic,
-    UserSchema,
-)
+from fastapi_zero.database import get_session
+from fastapi_zero.models import User
+from fastapi_zero.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI(title='FastAPI do Zero')
-
-
-database = []
 
 
 @app.get(
@@ -50,12 +45,33 @@ def ola_mundo_html():
     status_code=status.HTTP_201_CREATED,
     response_model=UserPublic,
 )
-def create_user(user: UserSchema):
-    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+def create_user(user: UserSchema, session: Session = Depends(get_session)):
+    db_user = session.scalar(
+        select(User).where(
+            (User.username == user.username) | (User.email == user.email)
+        )
+    )
 
-    database.append(user_with_id)
+    if db_user:
+        if db_user.username == user.username:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Username already exists',
+            )
+        elif db_user.email == user.email:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='Email already exists',
+            )
 
-    return user_with_id
+    db_user = User(
+        username=user.username, password=user.password, email=user.email
+    )
+    session.add(db_user)
+    session.commit()
+    session.refresh(db_user)
+
+    return db_user
 
 
 @app.get(
@@ -64,8 +80,11 @@ def create_user(user: UserSchema):
     status_code=status.HTTP_200_OK,
     response_model=UserList,
 )
-def read_users():
-    return {'users': database}
+def read_users(
+    skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
+):
+    users = session.scalars(select(User).offset(skip).limit(limit)).all()
+    return {'users': users}
 
 
 @app.get(
@@ -74,13 +93,13 @@ def read_users():
     status_code=status.HTTP_200_OK,
     response_model=UserPublic,
 )
-def read_one_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail='User not found!'
-        )
+def read_one_user(user_id: int, session: Session = Depends(get_session)):
+    user = session.scalar(select(User).where((User.id == user_id)))
 
-    user = next((user for user in database if user.id == user_id), None)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
+        )
 
     return user
 
@@ -91,16 +110,22 @@ def read_one_user(user_id: int):
     status_code=status.HTTP_200_OK,
     response_model=UserPublic,
 )
-def update_user(user_id: int, user: UserSchema):
-    if user_id > len(database) or user_id < 1:
+def update_user(
+    user_id: int, user: UserSchema, session: Session = Depends(get_session)
+):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
         )
 
-    user_with_id = UserDB(**user.model_dump(), id=user_id)
-    database[user_id - 1] = user_with_id
+    db_user.username = user.username
+    db_user.password = user.password
+    db_user.email = user.email
+    session.commit()
+    session.refresh(db_user)
 
-    return user_with_id
+    return db_user
 
 
 @app.delete(
@@ -109,12 +134,15 @@ def update_user(user_id: int, user: UserSchema):
     status_code=status.HTTP_200_OK,
     response_model=Message,
 )
-def delete_user(user_id: int):
-    if user_id > len(database) or user_id < 1:
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+
+    if not db_user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail='User not found'
         )
 
-    del database[user_id - 1]
+    session.delete(db_user)
+    session.commit()
 
     return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fastapi_zero.schemas import UserPublic
+
 
 def test_read_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')
@@ -15,7 +17,7 @@ def test_olamundo_deve_retornar_template_html_escrito_Ola_Mundo(client):
 
 def test_create_user(client):
     response = client.post(
-        '/users/',
+        '/users',
         json={
             'username': 'alice',
             'email': 'alice@example.com',
@@ -33,18 +35,16 @@ def test_create_user(client):
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_users_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
@@ -61,14 +61,14 @@ def test_update_user(client):
     }
 
 
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
 
 
-def test_if_user_not_found_on_put_404(client):
+def test_if_user_not_found_on_put_404(client, user):
     response = client.put(
         '/users/2',
         json={
@@ -80,22 +80,40 @@ def test_if_user_not_found_on_put_404(client):
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_if_user_not_found_on_delete_404(client):
+def test_get_one_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == user_schema
+
+
+def test_if_user_not_found_on_delete_404(client, user):
     response = client.delete('/users/2')
     assert response.status_code == HTTPStatus.NOT_FOUND
 
 
-def test_get_one_user(client):
-    test_create_user(client)
-    response = client.get('/users/1')
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'username': 'alice',
-        'email': 'alice@example.com',
-        'id': 1,
-    }
-
-
-def test_if_user_not_exist_404(client):
+def test_if_user_not_exist_404(client, user):
     response = client.get('/users/2')
     assert response.status_code == HTTPStatus.NOT_FOUND
+
+
+def test_create_new_user_verify_if_username_exists(client, user):
+    new_user_data = {
+        "username": user.username,
+        "email": "newteste@test.com",
+        "password": "newpassword"
+    }
+    response = client.post("/users", json=new_user_data)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Username already exists"}
+
+
+def test_create_new_user__verify_if_email_exists(client, user):
+    new_user_data = {
+        "username": "NewTeste",
+        "email": user.email,
+        "password": "newpassword"
+    }
+    response = client.post("/users", json=new_user_data)
+    assert response.status_code == HTTPStatus.BAD_REQUEST
+    assert response.json() == {"detail": "Email already exists"}
