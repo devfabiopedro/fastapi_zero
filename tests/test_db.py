af0a383af15e0740e4f8649edf6a542b8cfacd1b
devfabiopedro <fabio.silvapedro76@gmail.com>
2024-07-24 09:52:03 -0300

    teste database

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,5 +1,7 @@
 from sqlalchemy import select
+from sqlalchemy.orm import Session
 
+from fastapi_zero.database import get_session
 from fastapi_zero.models import Todo, User
 
 
@@ -27,3 +29,14 @@ def test_create_todo(session, user: User):
     user = session.scalar(select(User).where(User.id == user.id))
 
     assert todo in user.todos
+
+# (database) LN:10,11
+def test_get_session(session, engine, mocker):
+    mocker.patch('fastapi_zero.database.engine', engine)
+
+    session_generator = get_session()
+    generated_session = next(session_generator)
+
+    assert isinstance(generated_session, Session)
+    assert generated_session.bind.url == engine.url
+    session_generator.close()
